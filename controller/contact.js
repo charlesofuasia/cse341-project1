ea19f8b6170f5d270f9950a23796d58a185f6d1d
Charles Ofuasia <ofu23001@byui.edu>
2024-09-20 13:06:18 +0100

    get for one added

--- controller/contact.js
+++ controller/contact.js
@@ -1,24 +1,41 @@
 const mongodb = require("../database/dbconnection");
 const ObjectId = require("mongodb").ObjectId;
 
+
 const getContacts = async (req, res) => {
-   try {
-    const result = await mongodb.getDatabase().db().collection("contacts").find();
-    result.toArray().then((contacts)=> {
-        res.setHeader("Content-Type", "application/json");
-        res.status(201).json(contacts);
+    try {
+     const result = await mongodb.getDatabase().db().collection("contacts").find();
+     result.toArray().then((contacts)=> {
+         res.setHeader("Content-Type", "application/json");
+         res.status(201).json(contacts);
+ 
+     })
+ 
+    }catch(error){
+     console.error(error);
+ 
+    }
+ }
 
-    })
-
-   }catch(error){
-    console.error(error);
-
-   }
-}
-
+ const getOneContact = async (req, res) => {
+    try {
+        const userId = new ObjectId(req.params.id)
+     const result = await mongodb.getDatabase().db().collection("contacts").find({_id: userId});
+     result.toArray().then((contacts)=> {
+         res.setHeader("Content-Type", "application/json");
+         res.status(201).json(contacts[0]);
+ 
+     })
+ 
+    }catch(error){
+     console.error(error);
+ 
+    }
+ }
 
 
 
 module.exports = {
-    getContacts
+    getContacts,
+    getOneContact
 }
--- route/contacts.js
+++ route/contacts.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const contactController = require("../controller/contact")
 
-router.get("/", contactController.getContacts)
+router.get("/", contactController.getContacts);
+
+
+router.get("/:id", contactController.getOneContact);
 
 
 
