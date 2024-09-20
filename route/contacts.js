87d37c8904cf1909b8eeca13f2d3da562e42ad59
Charles Ofuasia <ofu23001@byui.edu>
2024-09-20 14:49:16 +0100

    formatted

--- route/contacts.js
+++ route/contacts.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
-const contactController = require("../controller/contact")
+const contactController = require("../controller/contact");
 
 router.get("/", contactController.getContacts);
 
-
 router.get("/:id", contactController.getOneContact);
 
-
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000
-const mongodb = require("./database/dbconnection")
+const PORT = process.env.PORT || 3000;
+const mongodb = require("./database/dbconnection");
 
-app.use("/", require("./route"))
+app.use("/", require("./route"));
 
-
-mongodb.initDB((error)=> {
-    if(error){
-        console.log(error)
-    }else{
-        app.listen(PORT, () => { console.log(`Database is connected and server is open at ${PORT}`) });
-    }
-})
-
+mongodb.initDB((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    app.listen(PORT, () => {
+      console.log(`Database is connected and server is open at ${PORT}`);
+    });
+  }
+});
