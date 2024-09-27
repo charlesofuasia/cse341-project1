b9ea4493f8e050785ebc3640df8545dc0b4a7f51
Charles Ofuasia <ofu23001@byui.edu>
2024-09-27 23:34:13 +0100

    Removed tags and descrption

--- route/contacts.js
+++ route/contacts.js
@@ -1,38 +1,13 @@
 const router = require("express").Router();
 const contactController = require("../controller/contact");
+router.get("/", contactController.getContacts);
 
-// #swagger.descriptions = "This end point retrieves all the contacts."
-router.get(
-  //  #swagger.tags = ["Contacts"]
-  "/",
-  contactController.getContacts
-);
-// #swagger.description = "Use the _id of a document to retrieve a contact"
-router.get(
-  // #swagger.tags = ["Contacts"]
-  "/:id",
-  contactController.getOneContact
-);
+router.get("/:id", contactController.getOneContact);
 
-// #swagger.description = "Add a new contact using json format"
-router.post(
-  // #swagger.tags = ["Contacts"]
-  "/",
-  contactController.addContact
-);
+router.post("/", contactController.addContact);
 
-// #swagger.description = "Update the details of a contact using the contact's _id"
-router.put(
-  // #swagger.tags = ["Contacts"]
-  "/:id",
-  contactController.updateContact
-);
+router.put("/:id", contactController.updateContact);
 
-// #swagger.description = "Delete contact from database using the contact's _id"
-router.delete(
-  // #swagger.tags = ["Contacts"]
-  "/:id",
-  contactController.deleteContact
-);
+router.delete("/:id", contactController.deleteContact);
 
 module.exports = router;
