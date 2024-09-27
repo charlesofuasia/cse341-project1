35902872efa46496b04a1e05e7d14ef9c41647be
Charles Ofuasia <ofu23001@byui.edu>
2024-09-27 21:48:59 +0100

    added descriptions

--- route/contacts.js
+++ route/contacts.js
@@ -1,37 +1,36 @@
 const router = require("express").Router();
 const contactController = require("../controller/contact");
 
+// #swagger.descriptions = "This end point retrieves all the contacts."
 router.get(
   //  #swagger.tags = ["Contacts"]
-  // #swagger.descriptions = "This end point retrieves all the contacts."
   "/",
   contactController.getContacts
 );
-
+// #swagger.description = "Use the _id of a document to retrieve a contact"
 router.get(
   // #swagger.tags = ["Contacts"]
-  // #swagger.description = "Use the _id of a document to retrieve a specific contact with matching _id"
   "/:id",
   contactController.getOneContact
 );
 
+// #swagger.description = "Add a new contact using json format"
 router.post(
   // #swagger.tags = ["Contacts"]
-  // #swagger.description = "Add a new contact using json format"
   "/",
   contactController.addContact
 );
 
+// #swagger.description = "Update the details of a contact using the contact's _id"
 router.put(
   // #swagger.tags = ["Contacts"]
-  // #swagger.description = "Update the details of a contact using the contact's _id"
   "/:id",
   contactController.updateContact
 );
 
+// #swagger.description = "Delete contact from database using the contact's _id"
 router.delete(
   // #swagger.tags = ["Contacts"]
-  // #swagger.description = "Delete contact from database using the contact's _id"
   "/:id",
   contactController.deleteContact
 );
