fdae8adef338e973869c92e9c087f4252a2efd57
Charles Ofuasia <ofu23001@byui.edu>
2024-09-27 15:48:59 +0100

    Enabled CORS in headers

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongodb = require("./database/dbconnection");
 const bodyParser = require("body-parser");
-
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 app.use("/", require("./route"));
 
 mongodb.initDB((error) => {
