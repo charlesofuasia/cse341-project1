d1af6099e7c92c7508577eae855dec9689060ded
Charles Ofuasia <ofu23001@byui.edu>
2024-09-28 09:19:03 +0100

    Re-added the tags & description. Seen the bug

--- route/contacts.js
+++ route/contacts.js
@@ -1,13 +1,39 @@
 const router = require("express").Router();
 const contactController = require("../controller/contact");
-router.get("/", contactController.getContacts);
 
-router.get("/:id", contactController.getOneContact);
+// #swagger.description = "This endpoint retrieves all the contacts"
+router.get(
+  // #swagger.tags = ["Contacts"]
+  "/",
+  contactController.getContacts
+);
 
-router.post("/", contactController.addContact);
+// #swagger.description = "This endpoint retrieves a specific contact using the contacts _id"
+router.get(
+  // #swagger.tags = ["Contacts"]
+  "/:id",
+  contactController.getOneContact
+);
 
-router.put("/:id", contactController.updateContact);
+// #swagger.description = "This endpoint adds a new contact to contacts"
+router.post(
+  // #swagger.tags = ["Contacts"]
+  "/",
+  contactController.addContact
+);
 
-router.delete("/:id", contactController.deleteContact);
+// #swagger.description = "Use this endpoint to modify the details of a contact using contact's _id"
+router.put(
+  // #swagger.tags = ["Contacts"]
+  "/:id",
+  contactController.updateContact
+);
+
+// #swagger.description = "Use this endpoint to delete contact from contacts using the contact's _id"
+router.delete(
+  // #swagger.tags = ["Contacts"]
+  "/:id",
+  contactController.deleteContact
+);
 
 module.exports = router;
